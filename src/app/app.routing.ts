7f1157cf66c19089390a6e09481bcf7d1df19cdd
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-10-18 00:14:35 -0300

    Importei o template e fiz funcionar só com o login e a home

---
+++ src/app/app.routing.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes =[
+    { path: 'home',             component: HomeComponent },
+    { path: 'login',          component: LoginComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+      useHash: false
+    })
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
