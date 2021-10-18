ce161d18038a8b98ca1db2bc394be63f5b719732
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-10-18 01:14:18 -0300

    arrumei um pouco a navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -41,20 +41,12 @@ export class NavbarComponent implements OnInit {
     isHome() {
         var titlee = this.location.prepareExternalUrl(this.location.path());
 
-        if( titlee === '#/home' ) {
+        if( titlee === '/home' ) {
             return true;
         }
         else {
             return false;
         }
     }
-    isDocumentation() {
-        var titlee = this.location.prepareExternalUrl(this.location.path());
-        if( titlee === '#/documentation' ) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
+    
 }
