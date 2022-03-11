673e8b69ccf5b96c62bb33c248213830029e1e67
CodeWracker <dev.rodrigofs@gmail.com>
2022-03-11 19:00:12 -0300

    fiz um rascunho da home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,11 +4,118 @@ import { Link, useHistory } from "react-router-dom";
 import "./styles.css";
 import api from "../../services/api";
 
+import default_plant_image from '../../assets/default_plant.jpg';
+
 export default function Home() {
 
     return (
-        <>
-            <h2>oi</h2>
-        </>
+        <div className="home-container">
+            <div className="results-box-header-container">
+                <div className="search">
+                    <p className="home-title">Busca por Filtro</p>
+                    <button className="home-search-button">Ver Lista de espécies</button>
+                </div>
+
+                <div className="filtros-container">
+
+
+
+                    <form className="filtro-container">
+                        <input className="input-filtro" type="text" placeholder="Filtro Usuário" />
+                    </form>
+                    <form className="filtro-container">
+                        <input className="input-filtro" type="text" placeholder="Filtro Especie" />
+                    </form>
+
+                </div>
+            </div>
+            <div className="results-container">
+                <div className="home-plant-details-container">
+                    <div className="plant-image-container">
+                        <img className="plant-image-home" src={default_plant_image} alt="" />
+                    </div>
+                    <div className="plant-info-container">
+                        <p>Araucaria</p>
+                        <p>rodrigo</p>
+                        <p>abc</p>
+                        <div className="plant-status-container"><p>Vivo</p>
+                            <p>12/10/2021</p>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="home-plant-details-container">
+                    <div className="plant-image-container">
+                        <img className="plant-image-home" src={default_plant_image} alt="" />
+                    </div>
+                    <div className="plant-info-container">
+                        <p>Araucaria</p>
+                        <p>rodrigo</p>
+                        <p>abc</p>
+                        <div className="plant-status-container"><p>Vivo</p>
+                            <p>12/10/2021</p>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="home-plant-details-container">
+                    <div className="plant-image-container">
+                        <img className="plant-image-home" src={default_plant_image} alt="" />
+                    </div>
+                    <div className="plant-info-container">
+                        <p>Araucaria</p>
+                        <p>rodrigo</p>
+                        <p>abc</p>
+                        <div className="plant-status-container"><p>Vivo</p>
+                            <p>12/10/2021</p>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="home-plant-details-container">
+                    <div className="plant-image-container">
+                        <img className="plant-image-home" src={default_plant_image} alt="" />
+                    </div>
+                    <div className="plant-info-container">
+                        <p>Araucaria</p>
+                        <p>rodrigo</p>
+                        <p>abc</p>
+                        <div className="plant-status-container"><p>Vivo</p>
+                            <p>12/10/2021</p>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="home-plant-details-container">
+                    <div className="plant-image-container">
+                        <img className="plant-image-home" src={default_plant_image} alt="" />
+                    </div>
+                    <div className="plant-info-container">
+                        <p>Araucaria</p>
+                        <p>rodrigo</p>
+                        <p>abc</p>
+                        <div className="plant-status-container"><p>Vivo</p>
+                            <p>12/10/2021</p>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="home-plant-details-container">
+                    <div className="plant-image-container">
+                        <img className="plant-image-home" src={default_plant_image} alt="" />
+                    </div>
+                    <div className="plant-info-container">
+                        <p>Araucaria</p>
+                        <p>rodrigo</p>
+                        <p>abc</p>
+                        <div className="plant-status-container"><p>Vivo</p>
+                            <p>12/10/2021</p>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
