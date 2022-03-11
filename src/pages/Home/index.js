d58239712c03de0d44cf123a1d0f1de46d865e0e
CodeWracker <dev.rodrigofs@gmail.com>
2022-03-11 16:37:44 -0300

    crei a estrutura básica do react

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+
+import "./styles.css";
+import api from "../../services/api";
+
+export default function Home() {
+
+    return (
+        <>
+            <h2>oi</h2>
+        </>
+    )
+}
---
+++ src/routes.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from './pages/Home';
+/*
+import Login from './pages/Login';                          
+import Register from './pages/Register';                    
+import Profile from './pages/Profile';
+import Produto from './pages/Product';
+import Carrinho from './pages/Carrinho'
+*/
+
+
+export default function Routes() {
+
+    return (
+
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Home} />
+            </Switch>
+        </BrowserRouter>
+
+    );
+}
